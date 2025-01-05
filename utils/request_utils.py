72c5626162d7be5b8e66158ed6ea9a353ba71966
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-01-04 20:39:21 -0500

    remove unused func

--- utils/request_utils.py
+++ utils/request_utils.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from typing import Any, Type
+from typing import Type
 from flask import request
 from pydantic import BaseModel
 
@@ -31,25 +31,3 @@ def marshal_with(model: Type[BaseModel]):
         return decorated_function
     return inner
 
-
-def _serialize(response: Any, model: Type[BaseModel]):
-    """Serialize a response dict into a pydantic model.
-
-    Attempts to parse a response dict into a pydantic model
-    and returns the model instance.
-    If it fails, it returns a 500 error with a message.
-    """
-
-    try:
-        return model.model_validate(response).model_dump()
-    except Exception as e:
-        return {"message": str(e)}, 500
-
-def serialize_with(model: Type[BaseModel]):
-    def inner(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            response, status = f(*args, **kwargs)
-            return _serialize(response, model), status
-        return decorated_function
-    return inner
