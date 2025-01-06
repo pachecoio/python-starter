f213913f1b165737743fbcd334272eb54433df6e
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-01-06 07:17:36 -0500

    include error handlers

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from werkzeug.exceptions import HTTPException
 
 
 def create_app():
     app = Flask(__name__)
+    register_error_handlers(app)
     register_blueprints(app)
     return app
 
@@ -11,3 +13,18 @@ def register_blueprints(app):
     from views import user
 
     app.register_blueprint(user.bp)
+
+
+def register_error_handlers(app):
+
+    @app.errorhandler(HTTPException)
+    def handle_http_exception(e):
+        return {"message": e.description}, e.code
+
+    @app.errorhandler(Exception)
+    def handle_exception(e):
+        return {"message": f"Internal server error: {str(e)}"}, 500
+
+    app.register_error_handler(HTTPException, handle_http_exception)
+    app.register_error_handler(Exception, handle_exception)
+
